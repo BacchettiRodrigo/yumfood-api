4be737c63aad5d45c599a0bb0ebd0ed2231c88d3
Rodrigo <rodrigobacchetti@brazzo.com.br>
2019-09-23 10:03:26 -0300

    First Commit

---
+++ src/main/java/br/com/reignited/yumfood/YumfoodApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.reignited.yumfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumfoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumfoodApiApplication.class, args);
+	}
+
+}
