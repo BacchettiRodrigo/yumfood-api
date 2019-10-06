cf96568a49b46d6227a91453a22206bd7ccd544d
Rodrigo <rodrigobacchetti@gmail.com>
2019-10-06 13:47:19 -0300

    CozinhaController e EstadoController

---
+++ src/main/java/br/com/reignited/yumfood/api/controller/CozinhaController.java
@@ -0,0 +1,30 @@
+package br.com.reignited.yumfood.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.reignited.yumfood.domain.model.Cozinha;
+import br.com.reignited.yumfood.domain.repository.CozinhaRepository;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	@GetMapping
+	public List<Cozinha> listar() {
+		return cozinhaRepository.listar();
+	}
+
+	@GetMapping("/{cozinhaId}")
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
+		return cozinhaRepository.buscar(cozinhaId);
+	}
+}
