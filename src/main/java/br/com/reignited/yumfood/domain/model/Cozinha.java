332a6f2767118311b47d9f7daf4fd3ff7a08614e
Rodrigo <rodrigobacchetti@brazzo.com.br>
2019-10-29 15:34:12 -0300

    commit

--- src/main/java/br/com/reignited/yumfood/domain/model/Cozinha.java
+++ src/main/java/br/com/reignited/yumfood/domain/model/Cozinha.java
@@ -1,56 +1,59 @@
 package br.com.reignited.yumfood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
 @Table(name = "cozinha")
 public class Cozinha {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@Column(name = "nome", length = 30, nullable = false)
-	private String nome;
+    @Column(name = "nome", length = 30, nullable = false)
+    private String nome;
 
-	public Cozinha() {
-	}
+    public Cozinha() {
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
-		Cozinha cozinha = (Cozinha) o;
-		return id.equals(cozinha.id);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cozinha cozinha = (Cozinha) o;
+        return id.equals(cozinha.id);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
-	@Override
-	public String toString() {
-		return "Cozinha{" +
-				"id=" + id +
-				", nome='" + nome + '\'' +
-				'}';
-	}
+    @Override
+    public String toString() {
+        return "Cozinha{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
