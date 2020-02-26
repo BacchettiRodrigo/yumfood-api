df00192b5de5731df6f5587ec885ca920295b1e6
BacchettiRodrigo <rodrigobacchetti@gmail.com>
2020-02-26 18:34:16 -0300

    Implementação de Cache HTTP Deep e Shallow Filters

--- src/main/java/br/com/reignited/yumfood/api/controller/FormaPagamentoController.java
+++ src/main/java/br/com/reignited/yumfood/api/controller/FormaPagamentoController.java
@@ -7,18 +7,24 @@ import br.com.reignited.yumfood.api.model.input.FormaPagamentoInput;
 import br.com.reignited.yumfood.domain.model.FormaPagamento;
 import br.com.reignited.yumfood.domain.service.FormaPagamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import javax.validation.Valid;
+import java.time.OffsetDateTime;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/formas-pagamento")
 public class FormaPagamentoController {
 
     @Autowired
-    private FormaPagamentoService service;
+    private FormaPagamentoService formaPagamentoService;
 
     @Autowired
     private FormaPagamentoModelAssembler assembler;
@@ -27,36 +33,79 @@ public class FormaPagamentoController {
     private FormaPagamentoInputDisassembler disassembler;
 
     @GetMapping
-    public List<FormaPagamentoModel> listar() {
-        return assembler.toCollectionModel(service.listar());
+    public ResponseEntity<List<FormaPagamentoModel>> listar(ServletWebRequest request) {
+
+        ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+
+        String eTag = "0";
+
+        OffsetDateTime dataUltimaAtualizacao = formaPagamentoService.dataUltimaAtualizacao();
+
+        if (dataUltimaAtualizacao != null) {
+            eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+        }
+
+        if (request.checkNotModified(eTag)) {
+            return null;
+        }
+
+        List<FormaPagamento> formaPagamentos = formaPagamentoService.listar();
+
+        List<FormaPagamentoModel> pagamentoModelList = assembler.toCollectionModel(formaPagamentos);
+
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+                .eTag(eTag)
+                .body(pagamentoModelList);
     }
 
     @GetMapping("/{formaPagamentoId}")
-    public FormaPagamentoModel buscar(@PathVariable Long formaPagamentoId) {
-        return assembler.toModel(service.buscar(formaPagamentoId));
+    public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId, ServletWebRequest request) {
+        ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+
+        String eTag = "0";
+
+        OffsetDateTime dataUltimaAtualizacao = formaPagamentoService.dataUltimaAtualizacaoById(formaPagamentoId);
+
+        if (dataUltimaAtualizacao != null) {
+            eTag = String.valueOf(dataUltimaAtualizacao.toEpochSecond());
+        }
+
+        if (request.checkNotModified(eTag)) {
+            return null;
+        }
+
+        FormaPagamento formaPagamento = formaPagamentoService.buscar(formaPagamentoId);
+
+        FormaPagamentoModel formaPagamentoModel = assembler.toModel(formaPagamento);
+
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+                .eTag(eTag)
+                .body(formaPagamentoModel);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public FormaPagamentoModel adicionar(@Valid @RequestBody FormaPagamentoInput input) {
         FormaPagamento formaPagamento = disassembler.toDomainModel(input);
-        return assembler.toModel(service.salvar(formaPagamento));
+        return assembler.toModel(formaPagamentoService.salvar(formaPagamento));
     }
 
     @PutMapping("{formaPagamentoId}")
     public FormaPagamentoModel atualizar
             (@PathVariable Long formaPagamentoId, @Valid @RequestBody FormaPagamentoInput input) {
 
-        FormaPagamento formaPagamento = service.buscar(formaPagamentoId);
+        FormaPagamento formaPagamento = formaPagamentoService.buscar(formaPagamentoId);
 
         disassembler.copyToDomainObject(input, formaPagamento);
 
-        return assembler.toModel(service.salvar(formaPagamento));
+        return assembler.toModel(formaPagamentoService.salvar(formaPagamento));
     }
 
     @DeleteMapping("/{formaPagamentoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletar(@PathVariable Long formaPagamentoId) {
-        service.excluir(formaPagamentoId);
+        formaPagamentoService.excluir(formaPagamentoId);
     }
 }
--- src/main/java/br/com/reignited/yumfood/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/br/com/reignited/yumfood/domain/repository/FormaPagamentoRepository.java
@@ -2,9 +2,17 @@ package br.com.reignited.yumfood.domain.repository;
 
 import br.com.reignited.yumfood.domain.model.FormaPagamento;
 import br.com.reignited.yumfood.domain.repository.custom.CustomJpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.OffsetDateTime;
 
 @Repository
 public interface FormaPagamentoRepository extends CustomJpaRepository<FormaPagamento, Long> {
 
+    @Query("select max(dataAtualizacao) from FormaPagamento")
+    OffsetDateTime getDataUltimaAtualizacao();
+
+    @Query("select dataAtualizacao from FormaPagamento where id = :formaPagamentoId")
+    OffsetDateTime getDataAtualizacaoById(Long formaPagamentoId);
 }
